5315efad9187cc6e24c18658f93bba9ce5bafd33
Md. Ashraful Alam <34173370+ashrafuldotinfo@users.noreply.github.com>
2020-05-31 02:56:19 +0600

    search action

--- EmployeeManagement/Controllers/ApiController.cs
+++ EmployeeManagement/Controllers/ApiController.cs
@@ -25,5 +25,27 @@ namespace EmployeeManagement.Controllers
             var empList = _employeeList.GetEmployees(true);
             return empList;
         }
+
+        [Produces("application/json")]
+        [HttpGet("search/{nm}")]
+        public async Task<IActionResult> Search(string nm, string em)
+        {
+            try
+            {
+                var getResult = _employeeList.GetEmployees()
+                    .Where(x => x.Name.Contains(nm))
+                    .Select(p => new
+                    {
+                        name = p.Name
+                    })
+                    .ToList();
+
+                return Ok(getResult);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
     }
 }
