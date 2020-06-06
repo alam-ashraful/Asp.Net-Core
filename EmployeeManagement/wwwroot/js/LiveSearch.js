f6f3d2a32f148384e25d675dedeabf5b109f74ff
Md. Ashraful Alam <34173370+ashrafuldotinfo@users.noreply.github.com>
2020-06-07 03:49:58 +0600

    called through a function

---
+++ EmployeeManagement/wwwroot/js/LiveSearch.js
@@ -0,0 +1,17 @@
+﻿var ajaxLiveSearch = function (id, reqUrl, cType) {
+    $("#" + id).keyup(function () {
+        $.ajax({
+            url: reqUrl + $(this).val(),
+            contentType: cType,
+            success: function (res) {
+                $(".fun-result").remove();
+                $.each(res, function (key, val) {
+                    $("#" + id).after("<a target='_blank' class='fun-result' href='/home/edit/" + val['id'] + "'>" + val['name'] + "</a>");
+                });
+            },
+            error: function () {
+                $(".fun-result").remove();
+            }
+        });
+    });
+};
